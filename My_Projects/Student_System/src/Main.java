1174c391a728869243077b39ee0508e1596381dc
iiiieeeeu <mamamamamamamama88664422@gmail.com>
2026-07-18 18:38:19 +0300

    Add Student System

--- My_Projects/Student_System/src/Main.java
+++ My_Projects/Student_System/src/Main.java
@@ -1,47 +1,60 @@
 package My_Projects.Student_System.src;
- import java.util.Scanner;
-
-import student;
- class student {
-    private String name;
-    private int score;
-    public void setName(String name) {
-        if (name != null && !name.trim().isEmpty()) {
-            this.name = name;
-        }else{
-            this.name = "Unknown";
-     }
-    }
-    public String getName() {
-        return name;
-    }
-    public void setScore(int score) {
-        if (score < 0 || score > 100) {
-            this.score = 0;
-        } else {
-            this.score = score;
+import java.util.Scanner;
+import java.util.Collections;
+import java.util.ArrayList;
+public class Main {
+    public static void main(String[] args) {
+        ArrayList <Student> students = new ArrayList<>();
+        Scanner input = new Scanner(System.in);
+        while (true) {
+            System.out.printf("\n----Student Management system----\\n" );
+            System.out.printf("1. add Student \n");
+            System.out.printf("2. View Student \n");
+            System.out.printf("3. Sort Student (Highest to Lowest)  \n");
+            System.out.printf("4. Exit  \n");
+            System.out.printf("Enter Your Choice :  \n");
+            int choice = input.nextInt();
+            switch (choice) {
+                case 1:
+                    input.nextLine();
+                    System.out.printf("Enter Student Name :  \n");
+                    String name = input.nextLine();
+                    System.out.printf("Enter Student Score :  \n");
+                    int score = input.nextInt();
+                    Student student = new Student(name, score);
+                    students.add(student);
+                    System.out.printf("Student Added Successfully (:  \n");
+                    break;
+                case 2:
+                    if (students.isEmpty()){
+                        System.out.printf("No Students found . please add some first (:  \n");
+                    }else{
+                        System.out.printf(" \n----Student List----- \n");
+                        System.out.printf("%-20s %-10s \n" , "Name", "score" );
+                        System.out.printf("------------------------------ \n");
+                        for (Student s : students) {
+                            System.out.printf("%-20s %-10d \n", s.getName(), s.getScore());
+                        }
+                    }
+                
+                    break;
+                
+                case 3:
+                    if (students.isEmpty()){
+                        System.out.printf("No Students to sort  \n");
+                    }else{
+                        Collections.sort(students);
+                        System.out.printf("Students sorted Successfully (Highest to Lowest)  \n");
+                        System.out.printf("Press 2 to view the sorted list (:  \n");
+                    }
+                    break;
+                case 4:
+                    System.out.printf("Exiting program . Goodbye (:  \n");
+                    return;
+                default:
+                    System.out.printf("Invalid Choice! Please try again.  \n");
+                    break;
+            }
         }
     }
-    public int getScore() {
-        return score;
-    }
-    public student (String name, int score) {
-        setName(name);
-        setScore(score);
-    }
-}
-public class Main{
-public static void main(String[] args) {
-    Scanner input = new Scanner(System.in);
-    System.out.println("Enter the name then the score of the first student:");
-    student student1 = new student(input.nextLine(), input.nextInt());
-    input.nextLine();
-    System.out.println("Name: " + student1.getName());
-    System.out.println("Score: " + student1.getScore());
-    System.out.println("Enter the name then the score of the second student:");
-    student student2 = new student(input.nextLine(), input.nextInt());
-    input.nextLine();
-    System.out.println("Name: " + student2.getName());
-    System.out.println("Score: " + student2.getScore());
-}
 }
---
+++ My_Projects/Student_System/src/Student.java
@@ -0,0 +1,36 @@
+package My_Projects.Student_System.src;
+
+ import java.util.Scanner;
+public class Student implements Comparable<Student>{
+     private String name;
+     private int score;
+     @Override
+        public int compareTo(Student other){
+            return Integer.compare(other.score, this.score);
+        }
+ 
+    public void setName(String name) {
+        if (name != null && !name.trim().isEmpty()) {
+            this.name = name;
+        }else{
+            this.name = "Unknown";
+     }
+    }
+    public String getName() {
+        return name;
+    }
+    public void setScore(int score) {
+        if (score < 0 || score > 100) {
+            this.score = 0;
+        } else {
+            this.score = score;
+        }
+    }
+    public int getScore() {
+        return score;
+    }
+    public Student (String name, int score) {
+        setName(name);
+        setScore(score);
+    }
+}
